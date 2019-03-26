77257edafa131f62af3a2ba96a1d6b492ba79446
Juan Sebastian Ramirez <jramirjs@onjci.com>
2019-03-26 14:57:30 -0500

    Remove previous actions and reducers.

--- src/ducks/berries/index.js
+++ src/ducks/berries/index.js
@@ -1,6 +1,5 @@
 import berriesReducer from "./reducers";
 
-// export { default as todosSelectors } from "./selectors";
 export { default as berriesOperations } from "./operations";
 export { default as berriesTypes } from "./types";
 
--- src/reducers/rootReducer.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import * as berryReducers from "../ducks/index";
-
-const rootReducer = combineReducers(berryReducers);
-export default rootReducer;
